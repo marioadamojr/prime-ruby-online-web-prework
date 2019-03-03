93ebea6e12ed31d8e082e6fa0555321aab06bc62
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 16:54:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  end
+  scale = (2...integer)
+  scale.each do |x|
+    if integer % x == 0
+      return false
+    end
+  end
+  true
+end
