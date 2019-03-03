c85b7601ef853f1c0823d01d646df7e34706b7db
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 16:52:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  end
+  scale = (2...integer)
+  scale.each do |x|
+    if integer % x == 0
+      return false
+    else
+      return true
+    end
+  end
+end
